360dc2e934a052567b43cf6df23c70dfc59661d9
samgans <antonzhiltsov2000@gmail.com>
2022-05-19 23:51:47 +0300

    Create basic geometry definitions

---
+++ src/simple_rtree/geometry.py
@@ -0,0 +1,32 @@
+from typing import Any, Dict, Optional, Tuple
+
+from shapely.geometry import Polygon, box
+from shapely.geometry.base import BaseGeometry
+
+
+class RtreeBoundingRectangle:
+
+    def __init__(self, geometry: Polygon):
+        self.geometry = geometry
+
+    @property
+    def area(self) -> float:
+        return self.geometry.area
+
+    @property
+    def bounds(self) -> Tuple[float, float, float, float]:
+        return self.geometry.bounds
+
+    def union(self, mbr: "RtreeBoundingRectangle") -> "RtreeBoundingRectangle":
+        return RtreeBoundingRectangle(
+            box(*self.geometry.union(mbr.geometry).bounds)
+        )
+
+
+class RtreeGeometry:
+
+    def __init__(self, geometry: BaseGeometry, mbr: RtreeBoundingRectangle,
+                 attributes: Optional[Dict[str, Any]] = None):
+        self.geometry = geometry
+        self.mbr = mbr
+        self.attributes = attributes
